a269a290686a9525b1248353386aa7ed8ce7b755
DESKTOP-OF00HHI\lime <jwkim@deonlab.co.kr>
2021-12-15 17:42:12 +0900

    수정

---
+++ src/main/java/com/jw/user/Application.java
@@ -0,0 +1,20 @@
+package com.jw.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.ApplicationPidFileWriter;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.PropertySource;
+
+@SpringBootApplication
+@ComponentScan("com.jw.user")
+@PropertySource("classpath:application-${spring.profiles.active:dev}.properties")
+public class Application {
+	
+	public static void main(String[] args) {
+		SpringApplication app = new SpringApplication(Application.class);
+		app.addListeners(new ApplicationPidFileWriter());
+		app.run(args);
+//		SpringApplication.run(Project1Application.class, args);
+	}
+}
