cf48b32bae7c5329de79f375803db91bcb9e5fc8
DESKTOP-OF00HHI\lime <jwkim@deonlab.co.kr>
2021-12-16 14:44:06 +0900

    21.12.16 중간 업데이트

--- src/main/java/com/jw/user/model/User.java
+++ src/main/java/com/jw/user/model/User.java
@@ -1,5 +1,17 @@
 package com.jw.user.model;
 
-public class User {
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
 
+@Getter
+@Setter
+@ToString
+public class User {
+	
+	private String id;
+	private String name;
+	private String email;
+	private String create_time;
+	
 }
--- src/main/java/com/jw/user/web/userRest.java
+++ src/main/java/com/jw/user/web/userRest.java
@@ -1,11 +1,17 @@
 package com.jw.user.web;
 
+import java.util.Date;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.jw.user.model.User;
 import com.jw.user.service.userService;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @RestController
 @RequestMapping({"/user","/user/v1"})
 public class userRest {
@@ -15,9 +21,17 @@ public class userRest {
 		this.service = service;
 	}
 	
-	@GetMapping("/")
-	public String home() {
-		return "index";
+	@GetMapping
+	public Object user() throws Exception{
+		log.debug("/user start");
+		
+		User user = new User();
+		user.setId("proj");
+		user.setName("김지우");
+		user.setEmail("abcd@gmail.com");
+		user.setCreate_time(String.valueOf(new Date()));
+		
+		return user;
 	}
 	
 	
