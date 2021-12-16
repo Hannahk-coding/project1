abfdc4b329b69cb1292738e522b602216df689f7
DESKTOP-OF00HHI\lime <jwkim@deonlab.co.kr>
2021-12-16 11:58:31 +0900

    properties 수정 기타 수정

---
+++ src/main/java/com/jw/user/model/User.java
@@ -0,0 +1,5 @@
+package com.jw.user.model;
+
+public class User {
+
+}
--- src/main/java/com/jw/user/web/userRest.java
+++ src/main/java/com/jw/user/web/userRest.java
@@ -1,6 +1,7 @@
 package com.jw.user.web;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,14 +11,15 @@ import com.jw.user.service.userService;
 @RequestMapping({"/user","/user/v1"})
 public class userRest {
 	
-//	@Autowired
-//	private userService userservice;
-	
 	private final userService service;
 	public userRest(final userService service) {
 		this.service = service;
 	}
 	
+	@GetMapping("/")
+	public String home() {
+		return "index";
+	}
 	
 	
 }
