b1e713b6c26508f8cbe9725b4b82a699941c078b
DESKTOP-OF00HHI\lime <jiu94@hanmail.net>
2021-12-16 17:55:04 +0900

    오류 수정(logback 수정 전)

--- src/main/java/com/jw/user/dao/UserDao.java
+++
@@ -1,8 +0,0 @@
-package com.jw.user.dao;
-
-import org.springframework.stereotype.Repository;
-
-@Repository
-public class UserDao {
-	
-}
--- src/main/java/com/jw/user/service/userService.java
+++ src/main/java/com/jw/user/service/userService.java
@@ -1,14 +1,9 @@
 package com.jw.user.service;
 
-import org.mybatis.spring.SqlSessionTemplate;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class userService {
-	
-	@Autowired
-	private SqlSessionTemplate sqlSession;
 	
 	
 	
