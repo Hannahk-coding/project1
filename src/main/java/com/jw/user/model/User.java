84cbd1af841baf6bc04f6a5397b96c7ca536bbe3
DESKTOP-OF00HHI\lime <jiu94@hanmail.net>
2021-12-21 18:15:34 +0900

    config(DataSourceConfig, WebMvcConfig) 추가, Interceptor 추가, mapper 테스트를 위한 Junit 추가

--- src/main/java/com/jw/user/model/User.java
+++ src/main/java/com/jw/user/model/User.java
@@ -9,6 +9,7 @@ import lombok.Data;
 @Builder
 public class User {
 	
+	private int user_seq;
 	private String id;
 	private String name;
 	private String email;
---
+++ src/test/java/com/jw/user/mapper/UserMapperTest.java
@@ -0,0 +1,33 @@
+package com.jw.user.mapper;
+
+import static org.junit.Assert.*;
+
+import java.util.List;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.jw.user.model.User;
+
+@Transactional
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class UserMapperTest {
+	
+	private UserMapper mapper;
+	
+	@Test
+	public void testGetUserList() {
+		
+		try {
+			List<User> userList = mapper.getUserList();
+			assertNotNull(userList);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+}
